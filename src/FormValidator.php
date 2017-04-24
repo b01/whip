86c7ab0af3be0f8aea7bdd269dd367959e66cef2
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2017-04-24 13:47:40 -0400

    Move the form validator interface to the root source directory.

---
+++ src/FormValidator.php
@@ -0,0 +1,17 @@
+<?php namespace Whip\Controllers;
+
+/**
+ * Interface FormValidator
+ *
+ * @package \Whip\Controllers
+ */
+interface FormValidator
+{
+    /**
+     * Perform validation.
+     *
+     * @param array $errors
+     * @return bool
+     */
+    public function isValid(array & $errors) : bool;
+}
