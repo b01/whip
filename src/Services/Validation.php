ae6256342ad02da26f46b2537b26b3029d58b0ce
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-10-30 08:55:25 -0400

    Added ValidationService.

---
+++ src/Services/Validation.php
@@ -0,0 +1,16 @@
+<?php namespace Whip\Services;
+
+/**
+ * Class Validation
+ *
+ * @package \Whip\Services
+ */
+class Validation
+{
+    /**
+     * Validation constructor.
+     */
+    public function __construct()
+    {
+    }
+}
